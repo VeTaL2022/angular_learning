888bec6960e5f276336b55d5d6d45f5e3008ead5
VeTaL2022 <vetal.kots2003@gmail.com>
2022-11-01 13:55:08 +0200

    initial commit

---
+++ src/app/configs/urls.ts
@@ -0,0 +1,7 @@
+import {environment} from "../../environments/environment";
+
+const {CarsV2API} = environment;
+
+export const urls = {
+  cars: `${CarsV2API}/cars`
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  CarsV2API: 'https://owu.linkpc.net/api/v2'
 };
 
 /*
