67ffcc52ecd380b1f2aa52d17ad91015639748df
VeTaL2022 <vetal.kots2003@gmail.com>
2022-11-01 21:42:37 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
-import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { CarsComponent } from './components/cars/cars.component';
-import { CarComponent } from './components/car/car.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {NgModule} from '@angular/core';
+
+import {CarsComponent} from './components/cars/cars.component';
+import {CarComponent} from './components/car/car.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
