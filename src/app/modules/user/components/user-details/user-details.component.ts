d470f387e776d056e7afda59fb5a87c336d4f948
VeTaL2022 <vetal.kots2003@gmail.com>
2022-11-01 10:16:23 +0200

    initial commit

--- src/app/modules/user/components/user-details/user-details.component.ts
+++ src/app/modules/user/components/user-details/user-details.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
 import {IUser} from "../../interfaces";
-import {UserService} from "../../services";
 
 @Component({
   selector: 'app-user-details',
@@ -12,12 +11,10 @@ import {UserService} from "../../services";
 export class UserDetailsComponent implements OnInit {
   selectedUser: IUser;
 
-  constructor(private activatedRoute: ActivatedRoute, private userService: UserService) {
+  constructor(private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({id}) => {
-      this.userService.getById(id).subscribe(value => this.selectedUser = value);
-    })
+    this.activatedRoute.data.subscribe(({user}) => this.selectedUser = user);
   }
 }
